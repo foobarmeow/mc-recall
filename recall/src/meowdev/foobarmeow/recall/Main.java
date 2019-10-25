acf378cca840d9a09cf1495c23168828d9adb0db
Caleb Davis <caleb.aaron.davis@gmail.com>
2019-10-25 09:53:56 -0700

    Added list command. Small fixes/updates.

--- recall/src/meowdev/foobarmeow/recall/Main.java
+++ recall/src/meowdev/foobarmeow/recall/Main.java
@@ -2,15 +2,23 @@ package meowdev.foobarmeow.recall;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.MemorySection;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.entity.Player;
-import org.bukkit.Location;
 
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Location;
 
 public class Main extends JavaPlugin {
 	
-	int stop;
 	static String commandCreateWaypoint = "waypoint";
+	static String commandCreateWaypointShort = "wp";
 	static String commandDeleteWaypoint = "del";
 	static String commandRecall = "recall";
 	static String commandRecallShort = "r";
@@ -35,7 +43,7 @@ public class Main extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!command.getName().equalsIgnoreCase("r")) {
-			sender.sendMessage("recall can only be used with 'r'");
+			sender.sendMessage(ChatColor.RED + "recall can only be used with 'r'");
 			return false;
 		}
 
@@ -43,44 +51,53 @@ public class Main extends JavaPlugin {
 			return false;
 		}
 
-		final Player player = (Player) sender;		
+		Player player = (Player) sender;		
 		
 		if (args.length == 0) {
 			// Send usage
-			player.sendMessage(this.usage());
+			player.sendMessage(ChatColor.BLUE + this.usage());
 			return true;
 		}
 		
 		// If this command is to create a waypoint
-		if (args[0].equalsIgnoreCase(this.commandCreateWaypoint)) {
+		if (args[0].equalsIgnoreCase(Main.commandCreateWaypoint) || args[0].equalsIgnoreCase(Main.commandCreateWaypointShort)) {
 			this.createWaypoint(player, args[1]);
 			return true;
 		}
 
 		// If this command is to delete a waypoint
-		if (args[0].equalsIgnoreCase(this.commandDeleteWaypoint)) {
+		if (args[0].equalsIgnoreCase(Main.commandDeleteWaypoint)) {
 			this.deleteWaypoint(args[1]);
-			player.sendMessage("Deleted waypoint " + args[1]);
+			player.sendMessage(ChatColor.BLUE + "Deleted waypoint " + args[1]);
 			return true;
 		}
 		
 		// If this command is to recall
-		if (args[0].equalsIgnoreCase(this.commandRecall) || args[0].equalsIgnoreCase(commandRecallShort)) {
+		if (args[0].equalsIgnoreCase(Main.commandRecall) || args[0].equalsIgnoreCase(Main.commandRecallShort)) {
 			// Find the location last set for this player's ID
 			if (this.readWaypointFromConfiguration(player.getDisplayName()) != null) {
 				Location oldLocation = player.getLocation();
 				player.teleport(this.readWaypointFromConfiguration(player.getDisplayName()));
 				this.saveWayopointToConfiguration(player.getDisplayName(), oldLocation);
+				player.sendMessage(ChatColor.BLUE + "Recalled to location before last teleport");
 				return true;
 			} else {
-				player.sendMessage("No known last location to recall to...");
+				player.sendMessage(ChatColor.RED + "No known last location to recall to...");
 				return true;
 			}
 		}
 
 		// If this command is to list
-		if (args[0].equalsIgnoreCase(this.commandList)) {
+		if (args[0].equalsIgnoreCase(Main.commandList)) {
 			// TODO
+			final StringBuilder listBuilder = new StringBuilder();
+			listBuilder.append("Waypoint List:\n");
+			Map<String, Object> waypointsMap = ((MemorySection) this.getConfig().get(Main.waypointsSection)).getValues(false);
+			waypointsMap.forEach((k, v) -> {
+				Location loc = (Location) v;
+				listBuilder.append(String.format("%s - %d, %d, %d\n", k, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+			});
+			player.sendMessage(ChatColor.BLUE + listBuilder.toString());
 			return true;
 		}
 		
@@ -89,6 +106,7 @@ public class Main extends JavaPlugin {
 			Location oldLocation = player.getLocation();
 			player.teleport(this.readWaypointFromConfiguration(args[0]));
 			this.saveWayopointToConfiguration(player.getDisplayName(), oldLocation);
+			player.sendMessage(ChatColor.BLUE + String.format("Teleported to waypoint %s", args[0]));
 			return true;
 		}
 
@@ -97,22 +115,23 @@ public class Main extends JavaPlugin {
 	}
 	
 	private String usage() {
-		return "To create a waypoint, type '/r <waypoint name>'.\nTo recall to a waypoint, type '/r <waypoint name>'.\nYour own recall waypoint is keyed on your name. Waypoints can be viewed with the 'list' command.";
+		return ChatColor.BLUE + "/r waypoint | wp <waypoint name> - create a waypoint\n/r <existing waypoint name> - teleport to existing waypoint\n/r recall | r - recall to last point before teleport.\n/r del <waypoint name> - delete an existing waypoint"; 
 	}
 	
 	private void createWaypoint(Player player, String name) {
 		// Guard against waypoint names being a command name
 		if (name.equalsIgnoreCase(commandCreateWaypoint) || 
+			name.equalsIgnoreCase(commandCreateWaypointShort) || 
 			name.equalsIgnoreCase(commandRecall) || 
 			name.equalsIgnoreCase(commandRecallShort) ||
 			name.equalsIgnoreCase(commandList)) {
-			player.sendMessage("Cannot create a waypoint with any reserved words: waypoint, recall, r");
+			player.sendMessage(ChatColor.RED + "Cannot create a waypoint with any reserved words: waypoint, recall, r");
 			return;
 		}
 		
 		Location loc = player.getLocation();
 		this.saveWayopointToConfiguration(name, loc);
-		player.sendMessage(String.format("Set waypoint %s to %d, %d, %d", name, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+		player.sendMessage(ChatColor.BLUE + String.format("Set waypoint %s to %d, %d, %d", name, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
 	}
 	
 	private void deleteWaypoint(String name) {
